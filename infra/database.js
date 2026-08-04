34b071285e236dd0048fc0174e8dafd0ec85d3c2
marcelo-felix <doutor793@gmail.com>
2026-08-04 08:11:04 -0300

    database.js more rebust to errors

--- infra/database.js
+++ infra/database.js
@@ -9,6 +9,14 @@ async function query(qs) {
     password: process.env.POSTGRES_PASSWORD,
   });
 
+  console.log("banco local: ", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+
   try {
     await client.connect();
     const resq = await client.query(qs);
